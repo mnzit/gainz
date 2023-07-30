cef8d935e52fedff995de03fa64bd6b1a901a959
Manjit Shakya <mnzitshakya@gmail.com>
2023-07-30 10:12:23 +0545

    changes

---
+++ src/app/app.component.ts
@@ -0,0 +1,80 @@
+import 'zone.js/dist/zone';
+import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+} from '@angular/forms';
+
+
+interface Transaction {
+  date?: Date;
+  startAmount?: number;
+  endAmount?: number;
+  profit?: number;
+  totalProfit?: number;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'gainz';
+
+  myForm!: FormGroup;
+  final: Transaction = {};
+  transactions: Transaction[] = [];
+
+  ngOnInit() {
+    this.myForm = new FormGroup({
+      principalAmount: new FormControl(100000),
+      interestRate: new FormControl(0.48),
+      months: new FormControl(2),
+      perDay: new FormControl(3),
+      startDate: new FormControl('2023-07-29'),
+    });
+
+    const { principalAmount, interestRate, months, perDay, startDate } =
+      this.myForm.value;
+    this.calculate(principalAmount, interestRate, months, perDay, startDate);
+
+    this.myForm.valueChanges.subscribe((changes) => {
+      const { principalAmount, interestRate, months, perDay, startDate } =
+        changes;
+      this.calculate(principalAmount, interestRate, months, perDay, startDate);
+    });
+  }
+
+  calculate(
+    principalAmount: number,
+    interestRate: number,
+    months: number,
+    perDay: number,
+    startDate: string
+  ) {
+    const endDate = new Date(startDate);
+    endDate.setMonth(endDate.getMonth() + months);
+    const currentDate = new Date(startDate);
+    this.transactions = [];
+    let totalProfit: number = 0;
+    while (currentDate <= endDate) {
+      let transaction: Transaction = {
+        date: new Date(currentDate.toISOString().slice(0, 10)),
+      };
+      let initialAmount = principalAmount;
+      for (let m = 1; m <= perDay; m++) {
+        principalAmount =
+          principalAmount + (principalAmount * interestRate) / 100;
+      }
+      transaction.startAmount = initialAmount;
+      transaction.endAmount = principalAmount;
+      transaction.profit = principalAmount - initialAmount;
+      totalProfit += transaction.profit;
+      transaction.totalProfit = totalProfit;
+      currentDate.setDate(currentDate.getDate() + 1);
+      this.transactions.push(transaction);
+    }
+    this.final = this.transactions[this.transactions.length - 1];
+  }
+}
